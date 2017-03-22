940f6d72247f13d7260d0d1198f10c90f0f80510
Zhixiang Li <Zhixiang Li@MININT-LM4LO1G>
2017-03-22 10:37:26 -0400

    Some fix to the analyzer, add new la5_gui

--- CS1120ZLCALA5/src/edu/wmich/cs1120/la5/ElevationAnalyzer.java
+++ CS1120ZLCALA5/src/edu/wmich/cs1120/la5/ElevationAnalyzer.java
@@ -4,34 +4,43 @@ import java.util.ArrayList;
 
 public class ElevationAnalyzer implements IRover{
 
+	private MapCreatorFromTxt mcft;
+	private MapCreatorFromDat mcfd;
+	private ArrayList<IArea> path;
+	private String analysis;
 	@Override
 	public ArrayList<IArea> getPath() {
 		// TODO Auto-generated method stub
-		return null;
+		return path;
 	}
 
 	@Override
 	public void setPath(ArrayList<IArea> path) {
 		// TODO Auto-generated method stub
-		
+		path = mcft.getScanner().getPath(startRow, startCol);
+		path = mcfd.getScanner().getPath(startRow, startCol);
 	}
 
 	@Override
 	public void analyzePath() {
 		// TODO Auto-generated method stub
-		
+		double averageElevation = 0;
+		for(int i=0; i<path.size();i++){
+			averageElevation += getPath().get(i).getElevation();
+		}
+		analysis = Double.toString(averageElevation/path.size());
 	}
 
 	@Override
 	public String getAnalysis() {
 		// TODO Auto-generated method stub
-		return null;
+		return analysis;
 	}
 
 	@Override
 	public void setAnalysis(String analysis) {
 		// TODO Auto-generated method stub
-		
+		this.analysis = analysis;
 	}
 
 }
--- CS1120ZLCALA5/src/edu/wmich/cs1120/la5/RadiationAnalyzer.java
+++ CS1120ZLCALA5/src/edu/wmich/cs1120/la5/RadiationAnalyzer.java
@@ -4,34 +4,43 @@ import java.util.ArrayList;
 
 public class RadiationAnalyzer implements IRover{
 
+	private MapCreatorFromTxt mcft;
+	private MapCreatorFromDat mcfd;
+	private ArrayList<IArea> path;
+	private String analysis;
 	@Override
 	public ArrayList<IArea> getPath() {
 		// TODO Auto-generated method stub
-		return null;
+		return path;
 	}
 
 	@Override
 	public void setPath(ArrayList<IArea> path) {
 		// TODO Auto-generated method stub
-		
+		path = mcft.getScanner().getPath(startRow, startCol);
+		path = mcfd.getScanner().getPath(startRow, startCol);
 	}
 
 	@Override
 	public void analyzePath() {
 		// TODO Auto-generated method stub
-		
+		double pathRadiation = 0;
+		for(int i=0; i<path.size();i++){
+			pathRadiation += getPath().get(i).getRadiation();
+		}
+		analysis = Double.toString(pathRadiation);
 	}
 
 	@Override
 	public String getAnalysis() {
 		// TODO Auto-generated method stub
-		return null;
+		return analysis;
 	}
 
 	@Override
 	public void setAnalysis(String analysis) {
 		// TODO Auto-generated method stub
-		
+		this.analysis = analysis;
 	}
 
 }

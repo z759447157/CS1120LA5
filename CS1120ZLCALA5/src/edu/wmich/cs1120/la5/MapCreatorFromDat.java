ecaaf1012738b3d97fa7d90d68849f564e39c86f
Zhixiang Li <Zhixiang Li@MININT-LM4LO1G>
2017-03-21 21:27:24 -0400

    Complete java code for reading the dat file

--- CS1120ZLCALA5/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
+++ CS1120ZLCALA5/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
@@ -1,25 +1,68 @@
 package edu.wmich.cs1120.la5;
 
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class MapCreatorFromDat implements IMapCreator{
 
+	private TerrainScanner scanner = new TerrainScanner();
+	private Area[][] area = new Area[10][10];
 	@Override
 	public void scanTerrain(String fileName, int threshold) throws IOException {
 		// TODO Auto-generated method stub
-		
+		FileInputStream file_in = new FileInputStream("Terrain.dat");
+        DataInputStream data_in = new DataInputStream (file_in );
+        int i = 0;
+		int n = 0;
+        String data;
+        String[] splitLine = new String[3];
+        double basicEnergyCost;
+		double elevation;
+		double radiation;
+        boolean endOfFile = false;
+        
+        while(!endOfFile){
+        	try{
+        		if(n == 10){
+    				n = 0;
+    				i++;
+    			}
+        		data=data_in.readUTF();
+        		splitLine = data.split(" ");
+    			basicEnergyCost = Double.parseDouble(splitLine[0]);
+    			elevation = Double.parseDouble(splitLine[1]);
+    			radiation = Double.parseDouble(splitLine[2]);
+    			if(radiation >= 0.5 || (radiation < 0.5 && elevation > threshold * 0.5)){
+    				Area highArea = new HighArea(basicEnergyCost,elevation,radiation);
+    				area[i][n] = highArea; 
+    			}
+    			else{
+    				Area lowArea = new LowArea(basicEnergyCost,elevation,radiation);
+    				area[i][n] = lowArea; 
+    			}
+    			n++;
+    			
+        	}
+        	
+        	catch(EOFException e){
+        		endOfFile=true;
+        	}
+        }
+        scanner.setTerrain(area);
 	}
 
 	@Override
 	public TerrainScanner getScanner() {
 		// TODO Auto-generated method stub
-		return null;
+		return scanner;
 	}
 
 	@Override
 	public void setScanner(TerrainScanner scanner) {
 		// TODO Auto-generated method stub
-		
+		this.scanner = scanner;
 	}
 
 }
